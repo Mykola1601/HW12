a1a17e839118e022f93722d6f895089914b0ffeb
Mykola1601 <nik160186@gmail.com>
2023-10-26 20:59:07 +0300

    try1

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 
 
-# HW 11 + 9
+# HW 12 
 
 from collections import UserDict
 from datetime import date, datetime
 import pickle
 import sys
 import re
-
 
 file_name = 'book.bin'
 
@@ -35,32 +34,59 @@ class Name(Field):
 
 class Birthday(Field):
     def __init__(self, value):
+        self.__value = None
         self.value = value
-  
 
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value: str) -> None:
+        birthday = datetime.strptime(str(value), '%Y-%m-%d')
+        birthday = birthday.date()
+        self.__value = birthday
+        if not self.__value:
+            raise ValueError  (f"Invalid  format  birthday")
+
+    def __str__(self):
+        return str(self.value)
+
+  
 class Phone(Field):
     def __init__(self, value):
-        super().__init__(value)
+        self.__value = None
+        self.value = value
+        # super().__init__(self.__value)
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value: str) -> None:
+        self.__value = normalize_phone(value)
+        if not self.__value:
+            raise ValueError  (f"Invalid phone number format != 10digit")
+
+    
 
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
-        self.__phones = []
-        self.__birthday = Birthday(None)
+        # self.__phones = []
+        self.phones = []
+        self.__birthday = None
 
-    @property
-    def phones(self):
-        return self.__phones
-
-    @phones.setter
     def add_phone(self, phone: str) -> None:
-        phone = normalize_phone(phone)
-        if not phone:
-            print (f"Invalid phone number format != 10digit")
-        else:
+        if phone not in self.phones:
             self.phones.append(Phone(phone))
+            return f"Phone number {phone} add to contact {self.name}"
+        print (f"Phone number {phone} present in contact {self.name}")
+
     
+
     def remove_phone(self, phone_number: str) -> None | str:
         for phone in self.phones:
             if phone.value == phone_number:
@@ -68,6 +94,7 @@ class Record:
                 break
         else:
             raise ValueError(f'phone {phone_number} not found ')
+
 
     def edit_phone(self, *args):
         old_phone = args[0]
@@ -103,21 +130,15 @@ class Record:
             return('No birthday date')
 
 
-    @property
-    def birthday(self):
-        return f'{self.name.value} birthday {self.__birthday.value}'
-
-    @birthday.setter
     def add_birthday(self, *args):
         try:
-            birthday = datetime.strptime(str(args[0]), '%Y-%m-%d')
-            self.__birthday = Birthday(birthday.date()) 
+            self.__birthday = Birthday(args[0]) 
         except ValueError:
             print('Error input - Enter birthday in format YYYY-mm-dd')
 
 
     def __str__(self):
-        return f"Contact: {self.name.value}; phones: {'; '.join(p.value for p in self.phones)}{'; Birthday '+ str(self.__birthday.value) if self.__birthday.value else '' }{';  '+ Record.days_to_birthday(self) if self.__birthday.value else '' }"
+        return f"Contact: {self.name.value}; phones: {'; '.join(p.value for p in self.phones)}{'; Birthday '+ str(self.__birthday.value) if self.__birthday else '' }{';  '+ Record.days_to_birthday(self) if self.__birthday else '' }"
 
 
 class AddressBook(UserDict):
@@ -180,17 +201,18 @@ def add(text=""):
     name = text.split()[0].title()    #get Name
     text = text.removeprefix(name.lower())    #remove Name
     if not len(text) >9:
-        return 'Enter valid  phone'
+        return 'Enter valid  phone 10dig'
     phone = normalize_phone(text)
+    # phone = text
     if not phone:
-        return 'Enter valid phone'
+        return 'Enter valid phone 10 dig'
     if not book.find(name):
         name = Record(name)
-        name.add_phone = phone
+        name.add_phone(phone)
         book.add_record(name)
         return name.name.value+" saved with number "+ phone
     name = book.find(name)
-    name.add_phone = phone
+    name.add_phone(phone)
     book.add_record(name)
     return  name.name.value+' added phone '+ phone
 
@@ -202,11 +224,11 @@ def birthday(text=""):
     name = text.split()[0].title()               #get Name
     text = text.removeprefix(name.lower())       #remove Name
     birthday = text.strip()
-    if book.data[name]:
-        book.data[name].add_birthday = (birthday)
+    if book.data.get(name):
+        book.data[name].add_birthday(birthday)
         return book.data[name]
     else:
-        return 'no'+name
+        return 'no '+name+' in book, add phone first'
 
 
 
@@ -226,7 +248,6 @@ def change(text=""):
         name = book.data[name]
         old_phone =  name.phones[0].value  #name.phones.value
         name.edit_phone(old_phone, phone)
-        # print(name)
         return name.name.value+" change number to "+ phone
     else:
         return (f"no {name} in phone book")
@@ -267,17 +288,49 @@ def show(text):
             count = counter
     return 'finish '
     
+# little HELP
+def help(_):
+    return """ 
+    "help" for help 
+    "add" for add contact
+    "find" for find in book
+    "hi" or "hello" for greeting
+    "birthday" to add birthday for exiting contact 
+    "change" for change number
+    "phone" for look  phone in contact
+    "show all" to show all book
+    "show" to show part of book    'show 2' - example
+    "exit" or "close" or "good bye" for exit and save book changes in file
+    """
 
 
-# exit program
+
+# iter in book and compare with FIND_text
+def find(text):
+    text = text.removeprefix("find ")
+    text = text.strip()
+    if not len(text) > 2:
+        return 'Enter more then 2 simbols to find'
+    list = ''
+    for cont in book:
+        if text in str(cont).lower():
+            list += str(cont) +'\r\n'
+    return list if len(list) > 1 else 'Cant find it'
+
+
+
+# exit program and save book to hhd
 def exit(_):
     with open(file_name, 'wb') as file:
         pickle.dump(book, file)
     return sys.exit('Good bye!\n')
 
 
+
 # dict for commands
 dic = { 
+    "help":help,
+    "find ":find,
     "hi":greeting,
     "hello":greeting,
     "birthday ":birthday,     #adding birthday
@@ -303,15 +356,17 @@ def find_command(text=""):
 
 
 book = AddressBook()
+
+# load contacts from file
 try:
     with open(file_name, 'rb') as file:
         book = pickle.load(file)
-        print('book loadeed')
+        print('Book loadeed from file ')
 except:
     print('No book file yet')
 
 
-
+# exit save book!!!
 def main():
     print("I'm Phone_Book_BOT, HELLO!!!")
     # loop forever
@@ -325,89 +380,9 @@ def main():
             print(out)
 
 
-
 # ========================================
 if __name__ == "__main__":
     main()
 
-
-
-
-
-
-
-    # john_record2 = Record("John1")
-    # john_record2.add_phone = ("1234567892")
-    # john_record3 = Record("John2")
-    # john_record3.add_phone = ("1234567893")
-    # john_record4 = Record("John3")
-    # john_record4.add_phone = ("1234567894")
-    # john_record4.add_birthday = ("1944-11-14")
-    # john_record5 = Record("John4")
-    # john_record5.add_phone = ("1234567895")
-    # john_record6 = Record("John5")
-    # john_record6.add_phone = ("1234567896")
-    # book.add_record(john_record2)
-    # book.add_record(john_record3)
-    # book.add_record(john_record4)
-    # book.add_record(john_record5)
-    # book.add_record(john_record6)
-
-
-    # john_record2 = Record("John2")
-    # john_record2.add_phone = ("1234567892")
-    # # Створення запису для John3
-    # john_record3 = Record("John3")
-    # john_record3.add_phone = ("1234567893")
-    # # Створення запису для John4
-    # john_record4 = Record("John4")
-    # john_record4.add_phone = ("1234567894")
-    # john_record4.add_birthday = ("1944-11-14")
-    # # Створення запису для John5
-    # john_record5 = Record("John5")
-    # john_record5.add_phone = ("1234567895")
-    # # Створення запису для John6
-    # john_record6 = Record("John6")
-    # john_record6.add_phone = ("1234567896")
-
-    # # Додавання запису John до адресної книги
-    # book.add_record(john_record)
-    # # Додавання запису John до адресної книги
-    # book.add_record(john_record2)
-    # # Додавання запису John до адресної книги
-    # book.add_record(john_record3)
-    # # Додавання запису John до адресної книги
-    # book.add_record(john_record4)
-    # # Додавання запису John до адресної книги
-    # book.add_record(john_record5)
-    # # Додавання запису John до адресної книги
-    # book.add_record(john_record6)
-
-    # # Створення та додавання нового запису для Jane
-    # jane_record = Record("Jane")
-    # jane_record.add_phone ="9876543210"
-    # # add birthday
-    # jane_record.add_birthday = ("1988-11-6")
-    # book.add_record(jane_record)
-
-    # # Виведення всіх записів у книзі
-    # for name, record in book.data.items():
-    #     print(record)
-
-    # # Знаходження та редагування телефону для John
-    # john = book.find("John")
-    # john.edit_phone("5555555555", "1112223333")
-
-    # print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
-
-
-    # # Пошук unknown телефону у записі John
-    # found_phone = john.find_phone("5555155555")
-    # print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
-
-    # # Пошук конкретного телефону у записі John
-    # found_phone = john.find_phone("1112223333")
-    # print(f"{john.name}: {found_phone}")  
-
-    # print('\r\n\n')
-    
+# "exit" or "close" or "good bye" for SAVE book changes in file !!!!!!!
+#  "help" for help
